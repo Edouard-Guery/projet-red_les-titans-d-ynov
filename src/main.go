07938d71ba4e718795c7a55c1a93aa9ef7261b93
Edouard GUERY <edouard.guery@ynov.com>
2026-09-21 10:36:47 +0200

    push edit main.go

--- src/main.go
+++ src/main.go
@@ -12,7 +12,7 @@ func main() {
 	var commande string
 
 	for {
-		fmt.Println("\nCommandes : start, inventaire, L'Échoppe de l'Olympe, forge de Héphaïstos, les potions de Dionysos, The Midas Club, info, stop, quit")
+		fmt.Println("\nCommandes : start, inventaire, shop, forge, potions, casino, info, stop, quit")
 		fmt.Print("> ")
 		fmt.Scan(&commande)
 		commande = strings.ToLower(commande)
@@ -22,19 +22,19 @@ func main() {
 			fmt.Println("\n[Combat] - Lancement...")
 		case "inv", "inventaire", "inv-list":
 			fmt.Printf("\nInventaire (%d/10) : %v\n", len(hero.Inventaire), hero.Inventaire)
-		case "magasin", "shop","L'Échoppe de l'Olympe":
+		case "magasin", "shop":
 			AfficherItem()
 			ChoisirItem(&hero, &Monstre)
-		case "forge", "craft","forge de Héphaïstos":
+		case "forge", "craft":
 			fmt.Println("\n[Forge] - À venir...")
-		case "potions", "potion","les potions de Dionysos":
+		case "potions", "potion":
 			// Appel du nouveau shop -- edouardo
 			hero.ShopPotions()
 		case "stop", "end":
 			fmt.Printf("\nStats - PV: %d | Attaque: %d | Défense: %d | Oboles: %d\n", hero.PV, hero.Attaque, hero.Defense, hero.Argent)
 		case "quit", "disconnect":
 			os.Exit(0)
-		case "casino", "The Midas Club":
+		case "casino":
 			casino(&hero)
 		case "info":
 			fmt.Printf("\nStats - PV: %d | Attaque: %d | Défense: %d | Oboles: %d\n", hero.PV, hero.Attaque, hero.Defense, hero.Argent)
